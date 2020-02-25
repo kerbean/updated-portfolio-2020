cf2b6b16b0f322d756ba0f7d51b221cf94cca81b
kerbean <kervindelrosario@yahoo.com>
2020-02-25 21:02:47 +0800

    UPDATED IMAGES

--- js/animation.js
+++ js/animation.js
@@ -5,3 +5,13 @@ AOS.init({
 });
 
 AOS.init();
+
+function redirect() {
+    event.preventDefault;
+    var strSubject = $("#contact-subject").val();
+    var strName = $("#contact-name").val();
+    var strMessage = $("#contact-body").val();
+    console.log(strSubject + " STRSUBJ");
+    window.location.href = "mailto:kervindelrosario@yahoo.com?subject=" + strSubject
+        + "&body=" + strMessage + "%0A%0ASincerely,%0A" + strName + "&cc=kervindelrosario@gmail.com";
+}
